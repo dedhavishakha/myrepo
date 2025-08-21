23bb7462cab3a62685bb5bd263672fdf6999c108
unknown <vishakhaaryavrat@gmail.com>
2025-08-21 11:22:18 +0530

    msg

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import TodoApp from './components/TodoApp'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <TodoApp/>
+    </>
+  )
+}
+
+export default App
